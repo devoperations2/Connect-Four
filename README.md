4848bbcc4933f7a58f513464bec3a67d0a06e166
devoperations <chrishayes0130@gmail.com>
2022-04-01 15:24:33 -0700

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+Here's our logo (hover to see the title text):
+
+Inline-style: 
+![alt text]https://i.imgur.com/0hSTC2g.jpg "Logo Title Text 1")
+
+Reference-style: 
+![alt text][logo]
+
+[logo]: https://i.imgur.com/0hSTC2g.jpg
---
+++ main.js
@@ -0,0 +1,23 @@
+// Define required variables used to track the state of the game
+// define variables for game board, winner/loser, 
+// define game with 42 squares and circles  
+// player to be able to click a square
+// define logic that winning equals four of the same color 
+// player to be able to click replay button
+
+
+
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
